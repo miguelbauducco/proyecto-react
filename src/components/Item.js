69427dd745e13eced51fe8cc7a3e318391f28f8a
miguelbauducco <miguel.bau14@gmail.com>
2022-10-18 19:09:25 -0300

    navbar modified

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,18 +2,26 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import '../styles/item.css'
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 const Item = ({item}) => {
 
 
     return (
-        <Link to={`/detail/${item.id}`}>
+        <Link className='link' to={`/detail/${item.id}`}>
             <div className='container'>
-                    <img className='element' src={item.img} alt='fotoproducto' style={{width:500, height: 300}}/>
-                    <li className='title'>{item.title}</li>
-                    <li className='element'>${item.price}</li>
+        <Card border="warning" bg="dark" style={{ width: '280px' }}>
+        <Card.Img variant="top" src={item.img} />
+        <Card.Body>
+        <Card.Title className='title'>{item.title}</Card.Title>
+        <Card.Text className='element'>
+        ${item.price}
+        </Card.Text>
+        <Button variant="warning">DETALLES</Button>
+        </Card.Body>
+        </Card>
             </div>
         </Link>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,24 @@
 import React from 'react'
 import '../styles/navbar.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav';
-import CartWidget from './CartWidget'
-import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
+import CartWidget from './CartWidget'
+
 
 
 const Navbartest = () => {
+
+
 return (
-
-    <Navbar> 
-        <Nav className='container'>
-
-            <Dropdown className='dropdownbox'>
-                <Dropdown.Toggle className='droptitle' variant="warning" id="dropdown-basic">
-                    Catalogo
-                </Dropdown.Toggle>
-                    <Dropdown.Menu className='dropdownmenu'>
-                        <Dropdown.Item className='d-flex justify-content-center'><Link to='/category/enterizas' className='nav-text-dropdown'>ENTERIZAS</Link></Dropdown.Item>
-                        <Dropdown.Item className='d-flex justify-content-center'><Link to='/category/bikini' className='nav-text-dropdown'>BIKINIS</Link></Dropdown.Item>
-                    </Dropdown.Menu>
-            </Dropdown>
-
-                    <Link to='/contact' className='nav-text'>
-                        Contacto
-                    </Link>
-                    <Link to='/cart'>
-                        <CartWidget/>
-                    </Link> 
-                </Nav> 
-            </Navbar>
-
-)
+    <div className='navbar'>
+        <Link className="navtext" style = {{textDecoration: "none", color: 'black', fontSize: "22px"}} to='/category'>Productos</Link>
+        <Link style = {{textDecoration: "none", color: 'black', fontSize: "22px"}} to='/category/bikini'>Bikinis</Link>
+        <Link style = {{textDecoration: "none", color: 'black', fontSize: "22px"}} to='/category/enterizas'>Enterizas</Link>  
+        <Link style = {{textDecoration: "none", color: 'black', fontSize: "22px"}} to='/contact'>Contacto</Link>
+        <CartWidget/>
+    </div>
+);
 }
+
+
 
 export default Navbartest;
