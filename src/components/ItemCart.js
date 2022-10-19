ea717d834a47eab9a18c7a44551d74c7de6f46a2
miguelbauducco <miguel.bau14@gmail.com>
2022-10-19 12:08:31 -0300

    adding css

--- src/components/ItemCart.js
+++ src/components/ItemCart.js
@@ -7,13 +7,15 @@ const ItemCart = ( {product} ) => {
 const {removeProduct} = useCartContext();
 
 return (
-    <div className='d-flex justify-content-center row'>  
+    <div className='container'>  
         <img className='img' src={product.img} alt='imgproduct'/>
+        <div className='col'>
         <h5 className='product-title'>{product.title}</h5>
         <div className='d-flex justify-content-around col'>
         <p className='quantity'>Cantidad: {product.quantity}</p>
         <p className='price'>Precio: ${product.price}</p>
         <p className='sub'>Subtotal: ${product.quantity * product.price}</p>
+        </div>
         <button className='btn-product' onClick = {() => removeProduct(product.id)}>Eliminar</button>
         </div>
     </div>
